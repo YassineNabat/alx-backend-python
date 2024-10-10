b35bb33ae5a99cf3dc9404801cbb2b4152546ae3
YassineNabat <nabatyassine1@gmail.com>
2024-10-10 15:00:22 +0100

    Updated

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,8 @@
 '''Task 0's module.
 '''
 
+
+
 def add (a: float, b: float) -> float:
     '''Adds two floating-point numbers.
     ''' 
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Task 2's module.
+'''
+
+
+
+def floor(n: float) ->int:
+    """returns the floor of the float
+    """
+    return int(n)
